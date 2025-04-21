0ad8a282e12aa285ef8449098d55150ba3f0d7d3
OrionGregory <orionalexanderg@gmail.com>
2025-04-20 21:24:10 -0500

    Update ALLOWED_HOSTS to allow all hosts for development

--- django-template/web_django/settings.py
+++ django-template/web_django/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'PLACEHOLDER')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 # Add CSRF_TRUSTED_ORIGINS setting
 CSRF_TRUSTED_ORIGINS = [
