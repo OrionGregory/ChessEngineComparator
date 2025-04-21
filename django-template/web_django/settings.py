bfc67205dadd65ac75e6ed0930f65136143d2c9c
OrionGregory <orionalexanderg@gmail.com>
2025-04-20 21:30:39 -0500

    Update ALLOWED_HOSTS to allow all hosts for development

--- django-template/web_django/settings.py
+++ django-template/web_django/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'PLACEHOLDER')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'chessenginecomparator-894499320278.us-central1.run.app']
 
 # Add CSRF_TRUSTED_ORIGINS setting
 CSRF_TRUSTED_ORIGINS = [
